73b3578785f06688e7e0e263375d564d2e5313a1
thehimel <himeldas66@gmail.com>
2021-03-16 23:12:46 +0100

    Create leap_year.py

---
+++ src/leap_year.py
@@ -0,0 +1,21 @@
+""""
+https://www.hackerrank.com/challenges/write-a-function
+
+Write a function to find if a year is leap year.
+"""
+
+
+def is_leap(year):
+    return year % 4 == 0 and (
+        year % 100 != 0 or (year % 100 == 0 and year % 400 == 0))
+
+
+assert is_leap(1990) is False
+assert is_leap(2004) is True
+assert is_leap(2008) is True
+assert is_leap(2024) is True
+assert is_leap(2036) is True
+assert is_leap(2038) is False
+
+# year = int(input())
+# print(is_leap(year=year))
