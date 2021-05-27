3d057b3c729fd738cc360ab340ad60d376eeed8b
Himel Das <60639301+thehimel@users.noreply.github.com>
2021-05-28 00:10:04 +0200

    Update input.py

--- src/input.py
+++ src/input.py
@@ -17,18 +17,29 @@ True
 
 Explanation:
 1**3 + 1**2 + 1 + 1 = 4
+
+Input:
+2 15
+x**4 - x + 1
+
+Output:
+True
+
+Explanation:
+2**4 - 2 + 1 = 15
 """
 
 
-def solution(x_k, equation):
-    # First item
-    x, k = int(x_k[0]), int(x_k[-1])  # noqa: F841
+def solution(xk, equation):
+    # Get a list of 2 numbers after the split
+    xk = list(map(int, xk.split()))
+    x, k = xk[0], xk[1]  # noqa: F841
 
     # Evaluating string equation
     return eval(equation) == k
 
 
 if __name__ == '__main__':
-    x_k = '1 4'
-    equation = 'x**3 + x**2 + x + 1'
-    print(solution(x_k, equation))
+    xk = '2 15'
+    equation = 'x**4 - x + 1'
+    print(solution(xk, equation))
